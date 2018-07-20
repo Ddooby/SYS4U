0d74bbe6fb0ad47c5403a216a829541b7fc54fcd
강대건 <kdg122.ext@lotte.net>
2018-07-20 13:42:39 +0900

    주임 진급시험 문제

--- src/main/java/sys4u/com/_2018/App.java
+++ src/main/java/sys4u/com/_2018/App.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -19,8 +21,14 @@ public class App
 		//--- 주임_진급자_대상_유형_1)_중복 문자열 갯수 출력
 		printDuplicatedChars( );
         
+		//--- 주임_진급자_대상_유형_2)_회문 체크 (거꾸로 뒤집어도 원래 문자열과 동일한지 체크)
+		System.out.println(isPalindrome("다시합시다") + "/" + isPalindrome("가나다라마"));
+		
         //--- 주임_진급자_대상_유형_3)_중복 문자열 제거
         printDuplicatedCharsDel();
+        
+        //--- 주임_진급자_대상_유형_4)_두 개의 문자열의 구성요소가 동일한지 확인하는 메소드
+        System.out.println(isIdenticalElements("aabeddew","wedab") + "/" + isIdenticalElements("acb","ca"));
 
     }
     
@@ -65,6 +73,12 @@ public class App
     	
     }
     
+    //--- 주임_진급자_대상_유형_2) 하나의 문자열이 회문인지 확인하는 메소드
+    private static boolean isPalindrome(String value) {
+    	String reversed = new StringBuffer(value).reverse().toString();
+    	return reversed.equals(value);
+    }
+    
     //--- 주임_진급자_대상_유형_3)_중복 문자열 제거
     //--- null 들어왔을때 exception 처리 필요
     private static void printDuplicatedCharsDel ( ) throws IllegalArgumentException {
@@ -91,4 +105,10 @@ public class App
     	resultList.forEach( i -> System.out.print( i ));
     }
 
+    //--- 주임_진급자_대상_유형_4) 두 개의 문자열의 구성요소가 동일한지 확인하는 메소드
+    private static boolean isIdenticalElements(String a, String b) {
+    	String aStream =  Arrays.asList(a.split("")).stream().distinct().sorted().collect(Collectors.joining());
+    	String bStream = Arrays.asList(b.split("")).stream().distinct().sorted().collect(Collectors.joining());
+    	return aStream.equals(bStream);
+    }
 }
